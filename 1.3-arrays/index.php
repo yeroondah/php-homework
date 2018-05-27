45b68277943e2e5a6f9447db2d8dfa795a1d3e27
Vladimir <vl.scherbitskiy@gmail.com>
2018-05-27 14:16:34 +0300

    внесены изменения

--- 1.3-arrays/index.php
+++ 1.3-arrays/index.php
@@ -1,68 +1,66 @@
 <?php
-    $animals = [
-        'Africa' => [
-            'Gekkonidae',
-            'Dendroaspis polylepis',
-            'Sagittarius serpentarius',
-            'Hyaenidae',
-            'Leptailurus serval'
-        ],
-        'Asia' => [
-            'Axis',
-            'Symphalangus syndactylus',
-            'Tarsius',
-            'Bos frontalis',
-            'Gavialis gangeticus'
-        ],
-        'Antarctica' => [
-            'Physeter macrocephalus',
-            'Lobodon carcinophagus',
-            'Rossella',
-            'Chionis alba',
-            'Pygoscelis papua'
-        ]
-    ];
+$animals = [
+    'Africa' => [
+        'Gekkonidae',
+        'Dendroaspis polylepis',
+        'Sagittarius serpentarius',
+        'Hyaenidae',
+        'Leptailurus serval'
+    ],
+    'Asia' => [
+        'Axis',
+        'Symphalangus syndactylus',
+        'Tarsius',
+        'Bos frontalis',
+        'Gavialis gangeticus'
+    ],
+    'Antarctica' => [
+        'Physeter macrocephalus',
+        'Lobodon carcinophagus',
+        'Rossella',
+        'Chionis alba',
+        'Pygoscelis papua'
+    ]
+];
+$namesFromTwoWords = [];
+$firstNames = [];
+$secondNames = [];
+$fantasyAnimals = [];
+$finalAnimalsList = [];
+$index = 0;
 
-    foreach ($animals as $continent => $animalsList) {
-        foreach ($animalsList as $animal) {
-            if (strpos($animal, ' ')) {
-                $namesFromTwoWords[$continent][] = explode(' ', $animal);
-            }   
-        }
+foreach ($animals as $continent => $animalsList) {
+    foreach ($animalsList as $animal) {
+        if (str_word_count($animal) == 2) {
+            $namesFromTwoWords[$continent][] = explode(' ', $animal);
+            $firstNames[] = $namesFromTwoWords[$continent][$index][0];
+            $secondNames[] = $namesFromTwoWords[$continent][$index][1];
+            $index++;
+       }   
     }
-
-    foreach ($namesFromTwoWords as $singleAnimal) {
-        foreach ($singleAnimal as $names) {
-            $firstNames[] = $names[0];
-            $secondNames[] = $names[1];
-        }
-    }
-
-    shuffle($firstNames);
-    shuffle($secondNames);
-
-    for ($index = 0; $index < count($firstNames); $index++) {
-        $fantasyAnimals[$index][0] = $firstNames[$index];
-        $fantasyAnimals[$index][1] = $secondNames[$index];
-    }
-
-    echo "<h2>Fantasy Animals</h2>";
-    for ($index = 0; $index < count($fantasyAnimals); $index++) {
-        echo "{$fantasyAnimals[$index][0]} {$fantasyAnimals[$index][1]}<br>";
-    }
-    
-    for ($i = 0; $i < count($fantasyAnimals); $i++) {
-        foreach ($namesFromTwoWords as $continent => $animal) {
-            for ($j = 0; $j < count($animal); $j++) {
-                if (in_array($fantasyAnimals[$i][0], $animal[$j])) {
-                    $finalAnimalsList[$continent][] = "{$fantasyAnimals[$i][0]} {$fantasyAnimals[$i][1]}";
-                }
+    $index = 0;
+}
+shuffle($firstNames);
+shuffle($secondNames);
+for ($index = 0; $index < count($firstNames); $index++) {
+    $fantasyAnimals[$index][0] = $firstNames[$index];
+    $fantasyAnimals[$index][1] = $secondNames[$index];
+}
+echo "<h2>Fantasy Animals</h2>";
+for ($index = 0; $index < count($fantasyAnimals); $index++) {
+    echo "{$fantasyAnimals[$index][0]} {$fantasyAnimals[$index][1]}<br>";
+}
+for ($i = 0; $i < count($fantasyAnimals); $i++) {
+    foreach ($namesFromTwoWords as $continent => $animal) {
+        for ($j = 0; $j < count($animal); $j++) {
+            if (in_array($fantasyAnimals[$i][0], $animal[$j])) {
+                $finalAnimalsList[$continent][] = "{$fantasyAnimals[$i][0]} {$fantasyAnimals[$i][1]}";
             }
         }
-    } 
-
-    foreach ($finalAnimalsList as $country => $animal) {
-        echo "<h2>$country</h2>";
-        echo implode(', ', $finalAnimalsList[$country]);
-    }  
+    }
+}
+foreach ($finalAnimalsList as $country => $animal) {
+    echo "<h2>$country</h2>";
+    echo implode(', ', $finalAnimalsList[$country]);
+}
 ?>
